9f062d0dd7f4ae561fe623d30117365b9809a8c0
Shawn W Dunn <sfalken@cloverleaf-linux.org>
2024-03-28 19:49:52 -0700

    Fixed typo in lib/kptyprocess.h

--- lib/kptyprocess.h
+++ lib/kptyprocess.h
@@ -145,7 +145,7 @@ protected:
     /**
      * @reimp
      */
-    virtual void setupChildProcess_() override;
+    virtual void setupChildProcess_();
 
 private:
     std::unique_ptr<KPtyProcessPrivate> const d_ptr;
